39f40355ba66de06a0610606ffb8547e1e64e3d9
Milad Khakpour <milad.khakpour@gmail.com>
2019-11-19 16:33:58 +0100

    inject service

--- src/main/java/com/mg/backend/GreetingResource.java
+++ src/main/java/com/mg/backend/GreetingResource.java
@@ -1,5 +1,8 @@
 package com.mg.backend;
 
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -7,6 +10,16 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class GreetingResource {
+
+    @Inject
+    GreetingService greetingService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/greet/{name}")
+    public String greeting(@PathParam String name) {
+        return greetingService.greeting(name);
+    }
     
     @GET
     @Produces(MediaType.TEXT_PLAIN)
