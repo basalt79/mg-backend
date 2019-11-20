0ffa8efc3f53df1c57b3788ebdde2a4c73a6952e
georg <georg@noenv.com>
2019-11-20 13:47:46 +0100

    added call sh to define the endpoints

---
+++ call.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+curl -X GET http://localhost:8080/greet
+#{"message":"Hello World!"}
+
+curl -X GET http://localhost:8080/greet/Joe
+#{"message":"Hello Joe!"}
+
+curl -X PUT -H "Content-Type: application/json" -d '{"greeting" : "Hola"}' http://localhost:8080/greet/greeting
+
+curl -X GET http://localhost:8080/greet/Jose
+#{"message":"Hola Jose!"}
